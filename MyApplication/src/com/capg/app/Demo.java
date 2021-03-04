cb63f8ea9ffb77250e3b5ca076e5ef7a95fe69eb
Monisha <Monisha@LAPTOP-0G85A5MS>
2021-03-04 12:28:37 +0530

    updated sample

--- MyApplication/src/com/capg/app/Demo.java
+++ MyApplication/src/com/capg/app/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("hello friends");
+		System.out.println("after change");
+		System.out.println("hii guys");
 
 	}
 
